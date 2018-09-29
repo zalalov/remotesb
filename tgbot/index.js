ccc959912064b68b16de9eec14c901a82b9df4e7
Ruslan Zalalov <zalalov@mail.ru>
2018-09-29 14:07:12 +0300

    Express.js app from "grabber" renamed to "back-end". New grabber component created.

--- tgbot/index.js
+++ tgbot/index.js
@@ -1,13 +1,23 @@
+#!/usr/local/bin/node
+
 const TelegramBot = require('node-telegram-bot-api');
+let schedule = require('node-schedule');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '<BOT_TOKEN>';
+const token = process.env.BOT_TOKEN;
+
+if (!token) {
+    console.error('BOT_TOKEN environment variable isn\'t specified.');
+    process.exit(1);
+}
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
+schedule.scheduleJob('*/5 * * * * *', async () => {
+    await bot.sendMessage('@remoterstest', 'Money.');
+});
 
-bot.sendMessage('@remoterstest', 'Money.');
 
 
 // // Matches "/echo [whatever]"
