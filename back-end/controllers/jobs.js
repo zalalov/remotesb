6b080935c94e5a7a45584dd81f5d67ccfa9d0551
Ruslan Zalalov <zalalov@mail.ru>
2018-10-09 22:37:54 +0300

    Jobs create now based on promises.

--- back-end/controllers/jobs.js
+++ back-end/controllers/jobs.js
@@ -2,27 +2,33 @@ let Job = require("../models/job");
 
 class JobsController {
     create(jobItem) {
-        let product = new Job(
-            {
-                title: jobItem.title,
-                description: jobItem.description,
-                company: jobItem.company,
-                created_at: jobItem.created_at,
+        return new Promise((resolve, reject) => {
+            let job = new Job(
+                {
+                    title: jobItem.title,
+                    description: jobItem.description,
+                    company: jobItem.company,
+                    created_at: jobItem.created_at,
 
-                service_id: jobItem.service_id,
-                service_item_id: jobItem.service_item_id,
-                service_published_at: jobItem.service_published_at,
-                service_link: jobItem.service_link
-            }
-        );
+                    service_id: jobItem.service_id,
+                    service_item_id: jobItem.service_item_id,
+                    service_published_at: jobItem.service_published_at,
+                    service_link: jobItem.service_link
+                }
+            );
 
-        product.save(function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log("Product Created successfully")
-            }
-        })
+            job.save((err) => {
+
+                if (err) {
+                    console.log(err);
+                    console.log("ERROR");
+                    reject(Error("Unable to save job item."))
+                } else {
+                    console.log("RESOLVED");
+                    resolve();
+                }
+            });
+        });
     }
 
     list(limit = 10) {
