6ce25d6467b77895afafef4d7d8e13f6d07185dc
Ruslan Zalalov <zalalov@mail.ru>
2018-10-12 01:45:06 +0300

    Index route file removed.

--- back-end/routes/index.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-
-/* GET home page. */
-router.get('/', async (req, res, next) => {
-    res.json({'status': 200})
-});
-
-module.exports = router;
