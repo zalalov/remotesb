cd8e85f9fb5e1d5557545a7249795050521f3409
Ruslan Zalalov <zalalov@mail.ru>
2018-10-11 22:55:58 +0300

    Job model's statuses added.

--- back-end/models/job.js
+++ back-end/models/job.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const STATUS_NEW = 100;
+const STATUS_READ = 200;
+const STATUS_QUEUED = 300;
+const STATUS_SENT = 400;
+
 // create a schema
 let jobSchema = new Schema({
     title: {type: String, required: true},
@@ -20,7 +25,6 @@ jobSchema.post('save', function (error, doc, next) {
     if (error.name === 'MongoError' && error.code === 11000) {
         next(new Error('Job item already exists.'));
     } else {
-
         console.log(`Job item saving post error handling: ${error}`);
 
         next(new Error('Unknown error.'));
